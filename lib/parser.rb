ce25a2cc1ee938e7230e11985d2cb9931c6e3be0
Chenbo Yi <yichenbo@outlook.com>
2019-07-03 11:05:53 +0100

    raise error when file does not exsit

--- lib/parser.rb
+++ lib/parser.rb
@@ -7,6 +7,8 @@ class Parser
   end
 
   def read_log(filename)
+    raise 'file does not exsit, please check again' unless File.file?(filename)
+
     File.open(filename, 'r') do |file|
       file.readlines.each do |line|
         url, ip = line.chomp.split(' ')
