044fcab986df4ef06ca233e2ddc71e67a3050594
Chenbo Yi <yichenbo@outlook.com>
2019-07-03 12:47:51 +0100

    refactor spec files

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -5,6 +5,13 @@ require 'parser'
 describe Parser do
   let(:printer) { double(:printer, print_in_visits_format: '', print_in_unique_views_format: '') }
   subject { described_class.new(printer) }
+  url1 = '/home'
+  url2 = '/contact'
+  url3 = '/index'
+  ip1 = '235.313.352.950'
+  ip2 = '184.123.665.067'
+  ip3 = '158.577.775.616'
+  ip4 = '722.247.931.582'
 
   before do
     allow(File).to receive(:file?).and_return(true)
@@ -14,23 +21,23 @@ describe Parser do
     context 'When file exsits and is not empty' do
       it 'transfer the log file data into a hash' do
         file1 = 'file1.log'
-        content1 = '/home 235.313.352.950'
+        content1 = "#{url1} #{ip1}"
         allow(File).to receive(:open).with(file1, 'r').and_yield(StringIO.new(content1))
-        expect(subject.read_log(file1)).to eq('/home' => ['235.313.352.950'])
+        expect(subject.read_log(file1)).to eq(url1 => [ip1])
       end
 
       it 'transfer another log file data into a hash' do
         file2 = 'file2.log'
-        content2 = '/contact 184.123.665.067'
+        content2 = "#{url2} #{ip2}"
         allow(File).to receive(:open).with(file2, 'r').and_yield(StringIO.new(content2))
-        expect(subject.read_log(file2)).to eq('/contact' => ['184.123.665.067'])
+        expect(subject.read_log(file2)).to eq(url2 => [ip2])
       end
 
       it 'add the ips to an array when visited the same page ' do
         file3 = 'file3.log'
-        content2 = "/home 184.123.665.067\n/home 235.313.352.950"
+        content2 = "#{url1} #{ip1}\n#{url1} #{ip2}"
         allow(File).to receive(:open).with(file3, 'r').and_yield(StringIO.new(content2))
-        expect(subject.read_log(file3)).to eq('/home' => ['184.123.665.067', '235.313.352.950'])
+        expect(subject.read_log(file3)).to eq(url1 => [ip1, ip2])
       end
     end
 
@@ -56,56 +63,45 @@ describe Parser do
   describe '#order_by_visits' do
     it 'sort the pages by number of total visits' do
       file6 = 'file6.log'
-      content6 = "/home 184.123.665.067\n/home 235.313.352.950\n/contact 184.123.665.067"
+      content6 = "#{url1} #{ip1}\n#{url1} #{ip2}\n#{url2} #{ip2}"
       allow(File).to receive(:open).with(file6, 'r').and_yield(StringIO.new(content6))
       subject.read_log(file6)
-      expect(subject.order_by_visits).to eq [['/home', ['184.123.665.067', '235.313.352.950']],
-                                             ['/contact', ['184.123.665.067']]]
+      expect(subject.order_by_visits).to eq [[url1, [ip1, ip2]], [url2, [ip2]]]
     end
 
     it 'sort the pages in alphabetical order when having same total visits' do
       file7 = 'file7.log'
-      content7 = "/home 184.123.665.067\n/home 235.313.352.950\n" \
-                 "/contact 184.123.665.067\n/index 158.577.775.616\n/index 722.247.931.582"
+      content7 = "#{url1} #{ip1}\n#{url1} #{ip2}\n#{url2} #{ip2}\n#{url3} #{ip3}\n#{url3} #{ip4}"
       allow(File).to receive(:open).with(file7, 'r').and_yield(StringIO.new(content7))
       subject.read_log(file7)
-      expect(subject.order_by_visits).to eq [['/home', ['184.123.665.067', '235.313.352.950']],
-                                             ['/index', ['158.577.775.616', '722.247.931.582']],
-                                             ['/contact', ['184.123.665.067']]]
+      expect(subject.order_by_visits).to eq [[url1, [ip1, ip2]], [url3, [ip3, ip4]], [url2, [ip2]]]
     end
   end
 
   describe '#order_by_uniq_views' do
     it 'sort the page by number of unique ips - no repeated ip' do
       file8 = 'file8.log'
-      content8 = "/home 184.123.665.067\n/home 235.313.352.950\n/contact 184.123.665.067"
+      content8 = "#{url1} #{ip1}\n#{url1} #{ip2}\n#{url2} #{ip2}"
       allow(File).to receive(:open).with(file8, 'r').and_yield(StringIO.new(content8))
       subject.read_log(file8)
-      expect(subject.order_by_uniq_views).to eq [['/home', ['184.123.665.067', '235.313.352.950']],
-                                                 ['/contact', ['184.123.665.067']]]
+      expect(subject.order_by_uniq_views).to eq [[url1, [ip1, ip2]], [url2, [ip2]]]
     end
 
     it 'sort the page by number of uniq ips - repeated ip' do
       file9 = 'file9.log'
-      content9 = "/home 235.313.352.950\n/home 235.313.352.950\n/home 235.313.352.950\n" \
-                 "/index 158.577.775.616\n/index 722.247.931.582"
+      content9 = "#{url1} #{ip1}\n#{url1} #{ip1}\n#{url1} #{ip1}\n#{url3} #{ip3}\n#{url3} #{ip4}"
       allow(File).to receive(:open).with(file9, 'r').and_yield(StringIO.new(content9))
       subject.read_log(file9)
-      expect(subject.order_by_uniq_views)
-        .to eq [['/index', ['158.577.775.616', '722.247.931.582']],
-                ['/home', ['235.313.352.950']]]
+      expect(subject.order_by_uniq_views).to eq [[url3, [ip3, ip4]], [url1, [ip1]]]
     end
 
     it 'sort the page by number of uniq ips and in alphabetical order  - repeated ip' do
       file10 = 'file10.log'
-      content10 = "/home 184.123.665.067\n/home 235.313.352.950\n/contact 184.123.665.067\n" \
-                 "/index 158.577.775.616\n/index 722.247.931.582"
+      content10 = "#{url1} #{ip1}\n#{url1} #{ip2}\n#{url2} #{ip2}\n#{url3} #{ip3}\n#{url3} #{ip4}"
       allow(File).to receive(:open).with(file10, 'r').and_yield(StringIO.new(content10))
       subject.read_log(file10)
       expect(subject.order_by_uniq_views)
-        .to eq [['/home', ['184.123.665.067', '235.313.352.950']],
-                ['/index', ['158.577.775.616', '722.247.931.582']],
-                ['/contact', ['184.123.665.067']]]
+        .to eq [[url1, [ip1, ip2]], [url3, [ip3, ip4]], [url2, [ip2]]]
     end
   end
 
