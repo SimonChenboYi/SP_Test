55923dfc303839342dac0179bab303eecb429355
Chenbo Yi <yichenbo@outlook.com>
2019-07-03 11:16:59 +0100

    extract the file_exsit? amd file_empty? methods to private

--- lib/parser.rb
+++ lib/parser.rb
@@ -7,16 +7,24 @@ class Parser
   end
 
   def read_log(filename)
-    raise 'file does not exsit, please check again' unless File.file?(filename)
-
+    file_exsit?(filename)
     File.open(filename, 'r') do |file|
       file.readlines.each do |line|
         url, ip = line.chomp.split(' ')
         @visits.key?(url) ? @visits[url].push(ip) : @visits[url] = [ip]
       end
     end
-    raise 'file is empty, please check again' if @visits.empty?
-
+    file_empty?
     @visits
+  end
+
+  private
+
+  def file_exsit?(filename)
+    raise 'file does not exsit, please check again' unless File.file?(filename)
+  end
+
+  def file_empty?
+    raise 'file is empty, please check again' if @visits.empty?
   end
 end
